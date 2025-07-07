cd83ce0c93d030fbfc7b587c51e4c21607a2aa30
FrancoLeuci <commercialfra@gmail.com>
2025-07-07 17:09:29 +0200

    Aggiunto module exports alle funzioni che ho scritto dei componenti

--- backend/controller/componentController.js
+++ backend/controller/componentController.js
@@ -1,10 +1,11 @@
 const Component=require("../model/componentModel")
 
 
+
 //funzione per aggiungere al database dei componenti un nuovo pezzo,
 // c'è già verifyRole come middleware
 async function newComponent(req,res){
-    const {category,imageUrl,nameTag,amount,priceTag}=req.body;
+    const {category,imageUrl,nameTag/*,amount*/,priceTag}=req.body;
     if(!category||!imageUrl||!nameTag||/*!amount||*/!priceTag){
         res.status(400).json({message:'Bisogna specificare necessariamente tutti i campi di un componente per poterlo inserire!'})
     }
@@ -70,4 +71,10 @@ async function getComponentByName(req,res){
         })
     }
 
+} //beta
+
+module.exports={
+    newComponent,
+    getAllComponents,
+    getComponentByName
 }
