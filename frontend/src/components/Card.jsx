37635d252b8df8e034de8b1b061b9366291ff3cd
FrancoLeuci <commercialfra@gmail.com>
2025-07-19 11:59:45 +0200

    Fix della Home

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -48,9 +48,11 @@ export default function Card({nome, imgURL, descrizione, prezzo, categoria, comp
                     <p>Prezzo</p>
                     <p>{prezzo}€</p>
                     <div className="d-flex gap-2">
-                        {userLogin !== null && <button className={"btn btn-primary"} onClick={handleAddToCart} value={componentID}>
+                        {userLogin !== null && (
+                            <button className={"btn btn-primary"} onClick={handleAddToCart} value={componentID}>
                             Aggiungi al Carrello
-                        </button>}
+                            </button>
+                        )}
 
                     </div>
                 </div>
